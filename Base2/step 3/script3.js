c92e56bd99d8c69bd7742d09f2be50c430f750d1
Aliosseily <ali.ossaily@outlook.com>
2018-02-20 17:20:08 +0200

    base2/script3

---
+++ Base2/step 3/script3.js
@@ -0,0 +1,14 @@
+console.log("third")
+
+var input = document.getElementById("name")
+
+
+var res = document.getElementById("result")
+
+function Show (){
+res.innerHTML=input.value
+
+
+}
+addEventListener("keyup",Show)
+ 
